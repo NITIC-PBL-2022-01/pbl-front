35adf249c26a6764c67f8b8ab2e973f250a3271e
isso0424 <isso04240129@gmail.com>
2022-12-09 15:58:53 +0900

    feat: add MonthIndicator

---
+++ components/Header/MonthIndicator.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import style from './MonthIndicator.module.css';
+
+interface Props {
+  year: number
+  month: number
+}
+
+export const MonthIndicator: FC <Props> = ({ year, month }) => {
+  return (
+    <div className={style.indicator}>
+      <span className={style.year}>{year}</span>
+      <span className={style.month}>{month}</span>
+    </div>
+  );
+};
