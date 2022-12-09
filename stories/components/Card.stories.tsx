3ba00d5a7a9277886e1fefeda63ffc42779c6ed8
isso0424 <isso04240129@gmail.com>
2022-12-09 13:52:27 +0900

    test: add card story

---
+++ stories/components/Card.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Card } from "../../components/common/Card";
+
+export default {
+  title: "Common/Card",
+  component: Card,
+};
+
+export const Default = () => (
+  <Card>Text</Card>
+);
