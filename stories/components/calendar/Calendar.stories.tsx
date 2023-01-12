b93cb7edd5d9299ab6be0c8072ecaded6cdfe3f1
isso0424 <isso04240129@gmail.com>
2023-01-13 03:03:38 +0900

    story: fix Calendar story

--- stories/components/calendar/Calendar.stories.tsx
+++ stories/components/calendar/Calendar.stories.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import React from "react";
 import { Calendar } from "../../../components/Calendar/Index";
 
@@ -7,9 +8,59 @@ export default {
 };
 
 export const Default = () => (
-  <Calendar
-    events={[{ title: "This is event", color: "#F00" }]}
-    year={2023}
-    month={2}
-  />
+  <div style={{ height: "90vh" }}>
+    <Calendar
+      events={[
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 4)),
+          time: "午前 5:00"
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 4)),
+          time: "午後 11:00"
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 8)),
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 12)),
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 16)),
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 23)),
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 23)),
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 23)),
+        },
+        {
+          title: "This is event",
+          color: "#F00",
+          date: dayjs(new Date(2023, 2, 28)),
+        },
+      ]}
+      year={2023}
+      month={2}
+    />
+  </div>
 );
