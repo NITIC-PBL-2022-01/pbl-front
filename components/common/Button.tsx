bf530c0c6a4b5ebcdf285b09e8b301c729a25151
isso0424 <isso04240129@gmail.com>
2023-01-16 13:54:56 +0900

    fix: button style

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -4,11 +4,18 @@ import styles from "./Button.module.css";
 interface Props {
   disabled?: boolean;
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
-const SharedButton: FC<Props & { className: string }> = ({ text, onClick, className, disabled }) => (
-  <button disabled={disabled} className={className} onClick={onClick}>{text}</button>
+const SharedButton: FC<Props & { className: string }> = ({
+  text,
+  onClick,
+  className,
+  disabled,
+}) => (
+  <button disabled={disabled} className={className} onClick={onClick}>
+    {text}
+  </button>
 );
 
 export const PrimaryButton: FC<Props> = (props) => (
