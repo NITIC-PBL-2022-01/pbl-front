55436a31e5a576097d0fca9c562de79e979445e3
isso0424 <isso04240129@gmail.com>
2023-01-16 11:36:21 +0900

    feat: simple button

---
+++ stories/components/Button.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { PrimaryButton, SecondaryButton } from "../../components/common/Button";
+
+export default {
+  title: "Common/Button",
+  component: PrimaryButton,
+};
+
+export const Primary = () => <PrimaryButton text="ボタン" onClick={() => console.log("clicked")} />;
+
+export const PrimaryDisabled = () => <PrimaryButton disabled text="ボタン" onClick={() => console.log("clicked")} />;
+
+export const Secondary = () => <SecondaryButton text="ボタン" onClick={() => console.log("clicked")} />;
+
+export const SecondaryDisabled = () => <SecondaryButton disabled text="ボタン" onClick={() => console.log("clicked")} />;
