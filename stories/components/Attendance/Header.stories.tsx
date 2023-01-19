680d653165e9d6759b897c8d5dfc2dc6a75fa5c3
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2023-01-19 23:47:15 +0900

    feat: student attendance (#13)

---
+++ stories/components/Attendance/Header.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../../components/Attendance/Header";
+
+export default {
+  title: "Attendance/Header",
+  component: Header,
+};
+
+export const Default = () => (
+  <Header />
+);
