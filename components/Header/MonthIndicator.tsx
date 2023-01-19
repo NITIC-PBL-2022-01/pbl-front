29bf388cc51866b40f9b64176c6542ccbc3cf45d
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2023-01-20 03:53:44 +0900

    feat: top page without api (#15)

--- components/Header/MonthIndicator.tsx
+++ components/Header/MonthIndicator.tsx
@@ -1,16 +1,23 @@
 import React, { FC } from "react";
+import { ArrowLeft, ArrowRight } from "react-feather";
 import style from "./MonthIndicator.module.css";
 
 interface Props {
   year: number;
   month: number;
+  incrementMonth: () => void;
+  decrementMonth: () => void;
 }
 
-export const MonthIndicator: FC<Props> = ({ year, month }) => {
+export const MonthIndicator: FC<Props> = ({ year, month, incrementMonth, decrementMonth }) => {
   return (
-    <div className={style.indicator}>
-      <span className={style.year}>{year}</span>
-      <span className={style.month}>{month}</span>
+    <div className={style.wrapper}>
+      <ArrowLeft size={32} className={style.button} onClick={decrementMonth} />
+      <div className={style.indicator}>
+        <span className={style.year}>{year}</span>
+        <span className={style.month}>{month}</span>
+      </div>
+      <ArrowRight size={32} className={style.button} onClick={incrementMonth} />
     </div>
   );
 };
